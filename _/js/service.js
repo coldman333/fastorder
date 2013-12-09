fe1799e7ed9f8cd23f5e8ce604e50b47d8d47a12
Bogdan <flashbogdan@gmail.com>
2013-12-09 23:26:32 +0200

    Коммит 02 =)

---
+++ _/js/service.js
@@ -0,0 +1,3 @@
+/**
+ * Created by BGDN on 09.12.13.
+ */
--- application/views/service_view.php
+++ application/views/service_view.php
@@ -1,26 +1,32 @@
         <header class="container">
             <section class="row">
-                <div class="col-lg-12">
+                <div class="col-md-12">
                     <a href="#" class="btn btn-default" id="refresh_btn"><i class="glyphicon glyphicon-refresh"></i> Обновить</a>
                 </div>
             </section>
         </header>
 
         <main class="container">
-            <section class="row order_item">
-                <div class="status_new">
+            <section class="row order">
+                <div class="row order_status_new">
+                    <div class="col-md-12"></div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-2">
-                         <p>123</p>
+                <div class="row order_data">
+                    <div class="col-md-1">
+                         <p>Столик №<span data-bind="text: ">9</span></p>
                     </div>
-                    <div class="col-lg-2">Order №: 234777438</div>
-                    <div class="col-lg-6">Contents: salad, asdas, asdasd, asdas</div>
-                    <div class="col-lg-2 order_item_controls">
+                    <div class="col-md-2">
+                        <p><i class="glyphicon glyphicon-cutlery"></i> <span data-bind="text: ">131209/012</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p>1) Lasagna 2) Soup</p>
+                    </div>
+                    <div class="col-md-3 col-sm-12 order_controls">
                         <div class="btn-group btn-group-justified">
-                            <a href="#" class="btn btn-default btn-success"><i class="glyphicon glyphicon-ok"></i></a>
-                            <a href="#" class="btn btn-default btn-warning"><i class="glyphicon glyphicon-pencil"></i></a>
-                            <a href="#" class="btn btn-default btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
+                            <a href="#" class="btn btn-default btn-primary" data-bind="click: "><i class="glyphicon glyphicon-eye-open"></i></a>
+                            <a href="#" class="btn btn-default btn-success" data-bind="click: "><i class="glyphicon glyphicon-ok"></i></a>
+                            <a href="#" class="btn btn-default btn-warning" data-bind="click: "><i class="glyphicon glyphicon-pencil"></i></a>
+                            <a href="#" class="btn btn-default btn-danger" data-bind="click: "><i class="glyphicon glyphicon-remove"></i></a>
                         </div>
                     </div>
                 </div>
